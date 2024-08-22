696d8e510de281afc4a8543b08fec55ce2f291f3
hhouaiss <hassan.houaiss@gmail.com>
2024-08-22 17:42:42 +0200

    update login button

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -79,7 +79,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
                     onClick={handleLogin}
                     className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
                   >
-                    Login with Google
+                    Login
                   </button>
                 </li>
               )}
